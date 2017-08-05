bbf37e0701487bbff7b1635ab62765b6746521d6
Tomo Furu <mokmok511@gmail.com>
2017-08-05 00:05:38 +0000

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'https://quiet-forest-58849.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host}
-  ActionMailer::Base.smtp_setting = {
+  ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
